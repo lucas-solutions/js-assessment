76e1403781cef28f395ee0e13a1f4675b38333c2
lucas-solutions <lucas@lucas.solutions>
2014-04-10 20:42:54 -0600

    count

--- app/count.js
+++ app/count.js
@@ -1,8 +1,32 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define(function () {
-  return {
-    count : function (start, end) {
-    }
-  };
+
+    var counter = function() {
+        this._current = -1;
+        this._running = false;
+        this._handler = null;
+    );
+
+    counter.prototype.count = function (start, end) {
+        this._running = true;
+        this._current = start;
+        console.log(this._current);
+        var that = this;
+        this._handler = setInterval(function () {
+            if  (that._running && that._current < end) {
+                that._current += 1;
+                console.log(that._current);
+            }
+        }, 100);
+        
+        return this;
+    };
+            
+    counter.prototype.cancel = function () {
+        this._running = false;
+        clearInterval(this._handler);
+    };
+            
+    return new counter();
 });
